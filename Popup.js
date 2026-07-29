321716fca4d2b6fe913e47add45024069cbd8d07
Kachimasu <winschauhan8@gmail.com>
2026-07-29 19:42:24 +0530

    Add script for opening and closing Discord popup

---
+++ Popup.js
@@ -0,0 +1,9 @@
+<script>
+function openDiscordPopup(){
+    document.getElementById("discordPopup").style.display="flex";
+}
+
+function closeDiscordPopup(){
+    document.getElementById("discordPopup").style.display="none";
+}
+</script>
